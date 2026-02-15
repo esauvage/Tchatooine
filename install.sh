e3e9ce56f5bc2e601a19260b1cef751892149ec8
Luke Skycodeur <niels@les-sauvages.fr>
2026-02-15 11:45:20 +0100

    Ajout du fichier install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [! -f "/bin/gcc" ]; then
+    echo "gcc n'est pas installé. Installation..."
+    sudo apt-get install gcc
+fi
+
+sudo apt-get install libnatpmp-dev
+
+gcc serveur.c chacha20-c/chacha20.c -o serveur -lnatpmp
